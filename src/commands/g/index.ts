03d34e31e0b7cc24db0ef99077674a2a30a1636a
feizheng <1290657123@qq.com>
2022-04-25 23:44:15 +0800

    refactor: update run fn

--- src/commands/g/index.ts
+++ src/commands/g/index.ts
@@ -27,19 +27,18 @@ export default class Index extends Command {
     const now = nx.Date.create(flags.date);
     const start = new Date(now.getFullYear(), now.getMonth() - 1, 25);
     const end = new Date(now.getFullYear(), now.getMonth(), 26);
-    const r1 = nx.rangeDate(
+    const range = nx.rangeDate(
       nx.Date.format(start, "date"),
       nx.Date.format(end, "date")
     );
-    const r1str = r1.map((date: any) => nx.Date.format(date, "date"));
-    const dates = nx.Collection.diff(r1str, vacation);
+    const rangeDates = range.map((date: any) => nx.Date.format(date, "date"));
+    const dates = nx.Collection.diff(rangeDates, vacation);
     const res = dates
       .map((date: string) => {
-        if (exchange.includes(date) || !nx.Date.isWeekend(date)) {
-          // 2.28 -
-          return nx.Date.format(date, "mm.dd") + " - " + "日志: xx";
-        }
-        return null;
+        const isWorkday = exchange.includes(date) || !nx.Date.isWeekend(date);
+        return isWorkday
+          ? nx.Date.format(date, "mm.dd") + " - " + "日志: xx"
+          : null;
       })
       .filter(Boolean);
     fs.writeFileSync(args.filename, res.join("\n"));
