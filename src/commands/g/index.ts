3ae22ba256aa7f7bda0e333f0b44187af8842f5b
feizheng <1290657123@qq.com>
2022-04-26 08:47:22 +0800

    chore: pre

--- src/commands/g/index.ts
+++ src/commands/g/index.ts
@@ -1,25 +1,23 @@
-import { Command, Flags } from "@oclif/core";
-import calendarVacation from "@jswork/calendar-vacation";
-import * as fs from "fs";
-import nx from "@jswork/next";
-import "@jswork/next-range-date";
-import "@jswork/next-date";
-import "@jswork/next-collection";
+import { Command, Flags } from '@oclif/core';
+import calendarVacation from '@jswork/calendar-vacation';
+import * as fs from 'fs';
+import nx from '@jswork/next';
+import '@jswork/next-range-date';
+import '@jswork/next-date';
+import '@jswork/next-collection';
 
 const { exchange, vacation } = calendarVacation();
 
 export default class Index extends Command {
-  static description = "Generate monthly.";
+  static description = 'Generate monthly.';
 
   static flags = {
-    format: Flags.string({ char: "f", description: "Date format.", default: "m.dd", }),
-    target: Flags.string({ char: "d", description: "Special day." }),
-    verbose: Flags.boolean({ char: "v", description: "Show logs." }),
+    format: Flags.string({ char: 'f', description: 'Date format.', default: 'm.dd' }),
+    target: Flags.string({ char: 'd', description: 'Special day.' }),
+    verbose: Flags.boolean({ char: 'v', description: 'Show logs.' }),
   };
 
-  static args = [
-    { name: "filename", description: "Create filename.", default: ".tmp.md" },
-  ];
+  static args = [{ name: 'filename', description: 'Create filename.', default: '.tmp.md' }];
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Index);
@@ -29,12 +27,12 @@ export default class Index extends Command {
       [target.getFullYear(), target.getMonth() + 1, 25]
     );
 
-    const rangeDates = range.map((date: any) => nx.Date.format(date, "date"));
+    const rangeDates = range.map((date: any) => nx.Date.format(date, 'date'));
     const dates = nx.Collection.diff(rangeDates, vacation);
     const res = dates
       .filter((date) => exchange.includes(date) || !nx.Date.isWeekend(date))
-      .map((date) => nx.Date.format(date, flags.format) + " - " + "日志: xx");
-    const result = res.join("\n");
+      .map((date) => nx.Date.format(date, flags.format) + ' - ' + '日志: xx');
+    const result = res.join('\n');
     flags.verbose && this.log(result);
     fs.writeFileSync(args.filename, result);
   }
